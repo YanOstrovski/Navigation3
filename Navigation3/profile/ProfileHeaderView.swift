26b5881b2ee3cc898e094b32bd7d7fd65d4ba231
JanOstrovski <dir@kbs41.ru>
2022-12-09 20:09:06 +1200

    HW5

--- Navigation3/profile/ProfileHeaderView.swift
+++ Navigation3/profile/ProfileHeaderView.swift
@@ -4,7 +4,7 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
-    var profileImageView: UIImageView = {
+    let avatarImageView: UIImageView = {
         
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
         imageView.image = UIImage(named: "HipsterCat")
@@ -18,10 +18,10 @@ class ProfileHeaderView: UIView {
         return imageView
     }()
     
-    var button: UIButton = {
+    let setStatusButton: UIButton = {
         let buttonView = UIButton()
         buttonView.translatesAutoresizingMaskIntoConstraints = false
-        buttonView.setTitle("Show status", for: .normal)
+        buttonView.setTitle("Set status", for: .normal)
         buttonView.setTitleColor(UIColor.white, for: .normal)
         buttonView.layer.backgroundColor = UIColor.blue.cgColor
         buttonView.layer.cornerRadius = 4
@@ -32,7 +32,7 @@ class ProfileHeaderView: UIView {
         return buttonView
     }()
     
-    var userName: UILabel = {
+    let fullNameLabel: UILabel = {
         let labelView = UILabel()
         labelView.translatesAutoresizingMaskIntoConstraints = false
         labelView.text = "Hipster Cat"
@@ -40,7 +40,7 @@ class ProfileHeaderView: UIView {
         return labelView
     }()
     
-    var userStatus: UILabel = {
+    let statusLabel: UILabel = {
         let labelView = UILabel()
         labelView.translatesAutoresizingMaskIntoConstraints = false
         labelView.text = "Waiting for something"
@@ -49,6 +49,15 @@ class ProfileHeaderView: UIView {
         return labelView
     }()
     
+    let statusTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Enter new status here"
+        textField.backgroundColor = UIColor.white
+        textField.borderStyle = .roundedRect
+        textField.isHidden = true
+        return textField
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -61,42 +70,60 @@ class ProfileHeaderView: UIView {
     
     
     func createScreenComponents() {
-        self.addSubview(profileImageView)
-        self.addSubview(button)
-        self.addSubview(userName)
-        self.addSubview(userStatus)
+        self.addSubview(avatarImageView)
+        self.addSubview(setStatusButton)
+        self.addSubview(fullNameLabel)
+        self.addSubview(statusLabel)
+        self.addSubview(statusTextField)
         addTargets()
         constraintElements()
     }
     
     func addTargets() {
-        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        setStatusButton.addTarget(self, action: #selector(changeStatus), for: .touchUpInside)
     }
     
     func constraintElements() {
         
-        profileImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 16).isActive = true
-        profileImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16).isActive = true
-        profileImageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        profileImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        avatarImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 16).isActive = true
+        avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16).isActive = true
+        avatarImageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        avatarImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
         
-        userName.topAnchor.constraint(equalTo: self.topAnchor, constant: 27).isActive = true
-        userName.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 16).isActive = true
-        userName.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        fullNameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 27).isActive = true
+        fullNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 16).isActive = true
+        fullNameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
         
-        userStatus.leadingAnchor.constraint(equalTo: profileImageView.trailingAnchor, constant: 16).isActive = true
-        userStatus.bottomAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: -16).isActive = true
-        userStatus.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        statusLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 16).isActive = true
+        statusLabel.bottomAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: -16).isActive = true
+        statusLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16).isActive = true
         
-        button.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 16).isActive = true
-        button.leadingAnchor.constraint(equalTo: profileImageView.leadingAnchor).isActive = true
-        button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        statusTextField.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 16).isActive = true
+        statusTextField.bottomAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: -16).isActive = true
+        statusTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16).isActive = true
+        
+        setStatusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 16).isActive = true
+        setStatusButton.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor).isActive = true
+        setStatusButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16).isActive = true
+        setStatusButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+       
         
     }
     
-    @objc func buttonPressed() {
-        print(userStatus.text ?? "")
+    @objc func changeStatus() {
+        //print(statusLabel.text ?? "")
+        
+        if (statusTextField.isHidden) {
+            statusTextField.text = statusLabel.text
+            statusTextField.isHidden = false
+            statusLabel.isHidden = true
+        } else {
+            statusLabel.text = statusTextField.text
+            statusTextField.isHidden = true
+            statusLabel.isHidden = false
+        }
+        
     }
 
     
